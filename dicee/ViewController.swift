bdf42cc818037315ab74c67b045b67b25b39d9ef
FG Dev Studio <49526345+ferdiangunawan@users.noreply.github.com>
2021-12-21 16:18:45 +0700

    Initial project

--- dicee/ViewController.swift
+++ dicee/ViewController.swift
@@ -4,16 +4,31 @@
 //
 //  Created by Ferdian Gunawan on 15/12/21.
 //
-
+ 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var logoFG2: UIImageView!
+    @IBOutlet weak var logoFG: UIImageView!
+    @IBAction func signInButton(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        logoFG.image = #imageLiteral(resourceName: "Logo FG 1")
+        logoFG2.image = #imageLiteral(resourceName: "D85")
     }
-
-
+    
+    @IBAction func changeImageButton(_ sender: UIButton) {
+        print("Button Pressed")
+        if (logoFG.image != #imageLiteral(resourceName:"D85")){
+            logoFG.image = #imageLiteral(resourceName: "D85")
+            logoFG2.image = #imageLiteral(resourceName: "Logo FG 1")
+        }else{
+            logoFG.image = #imageLiteral(resourceName: "Logo FG 1")
+            logoFG2.image = #imageLiteral(resourceName: "D85")
+        }
+       
+    }
 }
-
